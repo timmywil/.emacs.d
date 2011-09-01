911ce59b10f02be86323303ccda65536d930a59b
timmywil <timmywillisn@gmail.com>
2011-09-01 18:15:16 -0400

    Adding some css snippets

---
+++ textmate_import/.yas-setup.el
@@ -0,0 +1,24 @@
+;; .yas-setup.el for textmate_import
+;; 
+;; --**--
+;; Automatically generated code, do not edit this part
+;; 
+;; Translated menu
+;; 
+nil
+
+;; Unknown substitutions
+;; 
+;; Substitutions for: content
+;; 
+;; 
+
+;; Substitutions for: condition
+;; 
+;; 
+
+;; Substitutions for: binding
+;; 
+;; 
+
+;; .yas-setup.el for textmate_import ends here
